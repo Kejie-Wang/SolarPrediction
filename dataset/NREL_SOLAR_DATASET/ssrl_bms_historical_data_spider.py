5495b2eb9f5c5bbed68db384d5e7a5638f544c24
JackWang822 <wang_kejie@foxmail.com>
2016-12-01 20:07:58 +0800

    add the IR and METE data spider from the website and add the field of IR and METE and rename the two dataset name to IRANDMETE and SKYCAM

---
+++ dataset/NREL_SOLAR_DATASET/ssrl_bms_historical_data_spider.py
@@ -0,0 +1,33 @@
+import urllib2
+import os
+
+def get_file_name(year, month):
+	return str(year).rjust(4, '0') + str(month).rjust(2, '0') + ".zip"
+
+def download_file(url, file_path):
+	f = urllib2.urlopen(url)
+	with open(file_path, 'wb') as fp:
+		data = f.read()
+		fp.write(data)
+		fp.close()
+
+ys, ms = 2006, 1
+ye = 2016, 7
+
+url = 'https://www.nrel.gov/midc/srrl_bms/historical/data/'
+file_path = "./data/"
+
+if not os.path.exists(file_path):
+	os.mkdir(file_path)
+if not os.path.exists(file_path + str(ys)):
+	os.mkdir(file_path+str(ys))
+yit, mit = ys, ms
+while (yit < ye) or (yit==ye and mit<=ye):
+	file_name = get_file_name(yit, mit)
+	download_file(url+file_name, file_path+str(yit).rjust(4, '0')+'/'+file_name)
+	mit += 1
+	if mit > 12:
+		mit = 1
+		yit += 1
+		if not os.path.exists(file_path + str(yit).rjust(4, '0')):
+			os.mkdir(file_path + str(yit).rjust(4, '0'))
---
+++ dataset/NREL_SSRL_BMS_IRANDMETE/field_id/field_id_preprocess.py
@@ -0,0 +1,37 @@
+import numpy as np
+import operator
+
+field_id_0608 = np.loadtxt('200601_200806_id.csv', dtype='str', delimiter=',',comments=None)
+field_id_0812 = np.loadtxt('200807_201205_id.csv', dtype='str', delimiter=',',comments=None)
+field_id_1214 = np.loadtxt('201206_201412_id.csv', dtype='str', delimiter=',',comments=None)
+field_id_1516 = np.loadtxt('201501_201610_id.csv', dtype='str', delimiter=',',comments=None)
+
+field_id_0608_dict = dict(field_id_0608[:,[2,0]])
+field_id_0812_dict = dict(field_id_0812[:,[2,0]])
+field_id_1214_dict = dict(field_id_1214[:,[2,0]])
+field_id_1516_dict = dict(field_id_1516[:,[2,0]])
+
+meteorological_id = np.loadtxt('meteorological_id.csv', dtype='str', delimiter=',', comments=None)
+irradiance_id = np.loadtxt('irradiance_id.csv', dtype='str', delimiter=',', comments=None)
+
+field_id_common_label = np.intersect1d(np.intersect1d(field_id_0608[:,2], field_id_0812[:,2]), \
+                                np.intersect1d(field_id_1214[:,2], field_id_1516[:,2]))
+
+#get all common label the index in the field
+#assert all common filed id are with the same index in the filed id
+field_id_common_mete = dict()
+field_id_common_irra = dict()
+
+for id in field_id_common_label:
+    if id in meteorological_id:
+        field_id_common_mete[id] = field_id_0608_dict[id]
+    elif id in irradiance_id:
+        field_id_common_irra[id] = field_id_0608_dict[id]
+    else:
+        print id
+    assert field_id_0608_dict[id] == field_id_0812_dict[id] and \
+            field_id_0812_dict[id] == field_id_1214_dict[id] and \
+            field_id_1214_dict[id] == field_id_1516_dict[id]
+
+np.savetxt('meteorological_common_id.csv',  np.array(field_id_common_mete.items()), fmt='%s',  delimiter=',',comments=None)
+np.savetxt('irradiance_common_id.csv',  np.array(field_id_common_irra.items()), fmt='%s',  delimiter=',',comments=None)
