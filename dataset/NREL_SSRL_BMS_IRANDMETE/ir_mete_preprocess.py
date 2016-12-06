548f2a45a2640f6cdc635d4757f76e81294e87f1
JackWang822 <wang_kejie@foxmail.com>
2016-12-06 12:58:40 +0800

    move the part of the generating data into the input_data/generate.py

--- dataset/NREL_SSRL_BMS_IRANDMETE/ir_mete_preprocess.py
+++ dataset/NREL_SSRL_BMS_IRANDMETE/ir_mete_preprocess.py
@@ -9,7 +9,6 @@ __email__ = "wang_kejie@foxmail.com"
 import json
 from collections import namedtuple
 import numpy as np
-import time
 import zipfile
 from io import StringIO
 import os
@@ -73,52 +72,8 @@ ir_data = np.concatenate(ir_data, axis=0)
 mete_data = np.concatenate(mete_data, axis=0)
 target_data = np.concatenate(target_data, axis=0)
 
-#
-data_hour_length = len(ir_data)
-data_day_length = data_hour_length / HOUR_IN_A_DAY
-train_length = int(data_day_length * config.train_prop) * HOUR_IN_A_DAY
-validation_length = int(data_day_length * config.validation_prop) * HOUR_IN_A_DAY
-test_length = data_hour_length - train_length - validation_length
-
-ir_train_data = ir_data[0:train_length]
-ir_validation_data = ir_data[train_length:train_length+validation_length]
-ir_test_data = ir_data[train_length+validation_length:]
-
-mete_train_data = mete_data[0:train_length]
-mete_validation_data = mete_data[train_length:train_length+validation_length]
-mete_test_data = mete_data[train_length+validation_length:]
-
-target_train_data = target_data[0:train_length]
-target_validation_data = target_data[train_length:train_length+validation_length]
-target_test_data = target_data[train_length+validation_length:]
-
 if not os.path.exists(save_data_path):
 	os.mkdir(save_data_path)
-np.savetxt(save_data_path + 'ir_train_data.csv', ir_train_data, fmt='%.4f', delimiter=',')
-np.savetxt(save_data_path + 'ir_validation_data.csv', ir_validation_data, fmt='%.4f', delimiter=',')
-np.savetxt(save_data_path + 'ir_test_data.csv', ir_test_data, fmt='%.4f', delimiter=',')
-
-np.savetxt(save_data_path + 'mete_train_data.csv', mete_train_data, fmt='%.4f', delimiter=',')
-np.savetxt(save_data_path + 'mete_validation_data.csv', mete_validation_data, fmt='%.4f', delimiter=',')
-np.savetxt(save_data_path + 'mete_test_data.csv', mete_test_data, fmt='%.4f', delimiter=',')
-
-np.savetxt(save_data_path + 'target_train_data.csv', target_train_data, fmt='%.4f', delimiter=',')
-np.savetxt(save_data_path + 'target_validation_data.csv', target_validation_data, fmt='%.4f', delimiter=',')
-np.savetxt(save_data_path + 'target_test_data.csv', target_test_data, fmt='%.4f', delimiter=',')
-
-with open(save_data_path + 'README', 'w') as fp:
-	fp.write("SUMMARY\n")
-	fp.write("="*80)
-	fp.write("\n\n")
-
-	fp.write("This is an auto generate file by the pre-process file\n")
-	fp.write("author: WANG Kejie<wang_kejie@foxmail.com>\n")
-	fp.write("Generating time:"+time.strftime('%Y-%m-%d %X', time.localtime())+"\n")
-	fp.write("\n\n")
-
-	fp.write("Dataset Info\n")
-	fp.write("="*80 + "\n")
-	fp.write("The excel file is the source dataset and use the preprocess python script to generate the train, validation and test data\n")
-	fp.write("Train set data length: %d days / %d hours\n" %(train_length/HOUR_IN_A_DAY, train_length))
-	fp.write("Validation set data length: %d days / %d hours\n" %(validation_length/HOUR_IN_A_DAY, validation_length))
-	fp.write("Test set data length: %d days / %d hours\n" %(test_length/HOUR_IN_A_DAY, test_length))
+np.savetxt(save_data_path + 'ir_data.csv', ir_data, fmt='%.4f', delimiter=',')
+np.savetxt(save_data_path + 'mete_data.csv', mete_data, fmt='%.4f', delimiter=',')
+np.savetxt(save_data_path + 'target_data.csv', target_data, fmt='%.4f', delimiter=',')
