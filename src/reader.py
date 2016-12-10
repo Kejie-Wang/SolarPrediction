ab8f125d590d571bb244ca10d33307ab9976d04e
JackWang822 <wang_kejie@foxmail.com>
2016-12-10 11:47:37 +0800

    add the missing value handle

--- src/reader.py
+++ src/reader.py
@@ -4,18 +4,19 @@ from __future__ import division
 import numpy as np
 
 HOUR_IN_A_DAY = 24
+MISSING_VALUE = -99999
 
-ir_train_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/ir_train_data.csv"
-mete_train_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/mete_train_data.csv"
-target_train_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/target_train_data.csv"
+ir_train_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/train/ir_train_data.csv"
+mete_train_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/train/mete_train_data.csv"
+target_train_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/train/target_train_data.csv"
 
-ir_validation_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/ir_validation_data.csv"
-mete_validation_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/mete_validation_data.csv"
-target_validation_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/target_validation_data.csv"
+ir_validation_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/validation/ir_validation_data.csv"
+mete_validation_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/validation/mete_validation_data.csv"
+target_validation_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/validation/target_validation_data.csv"
 
-ir_test_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/ir_test_data.csv"
-mete_test_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/mete_test_data.csv"
-target_test_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/target_test_data.csv"
+ir_test_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/test/ir_test_data.csv"
+mete_test_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/test/mete_test_data.csv"
+target_test_data_path = "../dataset/NREL_SSRL_BMS_IRANDMETE/input_data/test/target_test_data.csv"
 
 
 class Reader:
@@ -40,6 +41,16 @@ class Reader:
             shape_targets.append(targets[ptr - n_target:ptr])
         return np.array(shape_targets)
 
+    def _get_valid_index(self, ir_features, mete_features, targets):
+        num = len(targets)
+        # print num
+        missing_index = []
+        for i in range(len(targets)):
+            if (MISSING_VALUE in ir_features[i]) or (MISSING_VALUE in mete_features[i]) or  (MISSING_VALUE in targets[i]):
+                missing_index.append(i)
+        # print missing_index, len(missing_index)
+        return np.setdiff1d(np.arange(num), np.array(missing_index))
+
     def __init__(self, config):
         """
         """
@@ -60,7 +71,7 @@ class Reader:
         ir_raw_data = np.concatenate((ir_train_raw_data, ir_validation_raw_data, ir_test_raw_data), axis=0)
         mete_raw_data = np.concatenate((mete_train_raw_data, mete_validation_raw_data, mete_test_raw_data), axis=0)
 
-        #scale on the all dataset
+        # scale on the all dataset
         # ir_raw_data_mean = np.mean(ir_raw_data, axis=0)
         # mete_raw_data_mean = np.mean(mete_raw_data, axis=0)
         # ir_raw_data_stddev = np.std(ir_raw_data, axis=0)
@@ -113,6 +124,10 @@ class Reader:
         self.target_validation_data = self._target_reshape(target_validation_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
         self.target_test_data = self._target_reshape(target_test_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
 
+        self.train_index = self._get_valid_index(self.ir_train_data, self.mete_train_data, self.target_train_data)
+        self.validation_index = self._get_valid_index(self.ir_validation_data, self.mete_validation_data, self.target_validation_data)
+        self.test_index = self._get_valid_index(self.ir_test_data, self.mete_test_data, self.target_test_data)
+
         #CAUTIOUS: the length of the ir_tarin_data and target_train_data may be differnet
         #the length of mete_test_data may be more short
         #and thus we must use the target data to compute the number
@@ -130,7 +145,7 @@ class Reader:
         print "validation number:", self.validataion_num
         print "test number", self.test_num
         print "batch size:", self.batch_size
-        print "use ", config.n_step, "hours to predict the next ", config.n_target, " consecutive hours"
+        print "use", config.n_step, "hours to predict the next ", config.n_target, " consecutive hours"
         print "\n\n"
 
     def next_batch(self):
@@ -139,7 +154,8 @@ class Reader:
         @return mete_data_batch:  [batch_size, n_step, n_input]
         @return target_data_batch: [n_model, batch_size, n_target]
         """
-        index = np.random.random_integers(0, self.train_num-1, size=(self.batch_size))
+        index = np.random.choice(self.train_index, self.batch_size)
+        # index = np.random.random_integers(0, self.train_num-1, size=(self.batch_size))
         ir_batch_data = self.ir_train_data[index]
         mete_batch_data = self.mete_train_data[index]
         target_batch_data = self.target_train_data[index]
@@ -147,9 +163,9 @@ class Reader:
         return ir_batch_data, mete_batch_data, target_batch_data
 
     def get_train_set(self):
-        return self.ir_train_data[0:self.train_num], \
-                self.mete_train_data[0:self.train_num], \
-                self.target_train_data[0:self.train_num]
+        return self.ir_train_data[self.train_index], \
+                self.mete_train_data[self.train_index], \
+                self.target_train_data[self.train_index]
 
     #The returned validataion and test set:
     #ir_data and mete_data: [batch_size, n_step, n_input], batch_size = validation_num/test_num
