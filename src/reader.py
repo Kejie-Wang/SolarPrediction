2b1a9de775a20f6540928cd4fd57b442651a527e
JackWang822 <wang_kejie@foxmail.com>
2017-01-03 14:01:42 +0800

    fix the bugs in feature scale and add the feature enhacement with adding some gaussian noise to the source train data

--- src/reader.py
+++ src/reader.py
@@ -63,6 +63,16 @@ class Reader:
         print missing_index
         return np.setdiff1d(np.arange(num), np.array(missing_index))
 
+    def _add_noise_to_data(self, data, expand_rate, stddev, no_noise_index=None):
+        shape = data.shape
+        l = [data]
+        for i in range(expand_rate):
+            noise = np.random.normal(0, stddev, shape)
+            if no_noise_index is not None:
+                noise[:, no_noise_index] = 0
+            l.append(data+noise)
+        return np.concatenate(l, axis=0)
+
     def __init__(self, config):
         """
         The constructor of the class Reader
@@ -71,8 +81,6 @@ class Reader:
         and do the feature pre-processing (now just scale the feature into mean is 0 and stddev is 1.0)
         """
 
-        ir_index = np.array([0, 1, 2, 17, 29, 35])
-        mete_index = np.array([1, 4, 7, 8, 9, 10, 16, 23, 24])
         #load data
         ir_train_raw_data = np.loadtxt(ir_train_data_path, delimiter=',', ndmin=2)
         ir_validation_raw_data = np.loadtxt(ir_validation_data_path, delimiter=',', ndmin=2)
@@ -89,55 +97,93 @@ class Reader:
         #feature reshape
         #feature reshape: accumulate several(n_step) features into a new feature for the input the lstm
         #target reshape: align the target with the input feature
-        self.ir_train_data = self._feature_reshape(ir_train_raw_data, config.data_step, config.n_step)
-        self.ir_validation_data = self._feature_reshape(ir_validation_raw_data, config.data_step, config.n_step)
-        self.ir_test_data = self._feature_reshape(ir_test_raw_data, config.data_step, config.n_step)
+        ir_train_data = self._feature_reshape(ir_train_raw_data, config.data_step, config.n_step)
+        ir_validation_data = self._feature_reshape(ir_validation_raw_data, config.data_step, config.n_step)
+        ir_test_data = self._feature_reshape(ir_test_raw_data, config.data_step, config.n_step)
 
-        self.mete_train_data = self._feature_reshape(mete_train_raw_data, config.data_step, config.n_step)
-        self.mete_validation_data = self._feature_reshape(mete_validation_raw_data, config.data_step, config.n_step)
-        self.mete_test_data = self._feature_reshape(mete_test_raw_data, config.data_step, config.n_step)
+        mete_train_data = self._feature_reshape(mete_train_raw_data, config.data_step, config.n_step)
+        mete_validation_data = self._feature_reshape(mete_validation_raw_data, config.data_step, config.n_step)
+        mete_test_data = self._feature_reshape(mete_test_raw_data, config.data_step, config.n_step)
 
-        self.target_train_data = self._target_reshape(target_train_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
-        self.target_validation_data = self._target_reshape(target_validation_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
-        self.target_test_data = self._target_reshape(target_test_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
+        target_train_data = self._target_reshape(target_train_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
+        target_validation_data = self._target_reshape(target_validation_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
+        target_test_data = self._target_reshape(target_test_raw_data, config.data_step, config.n_step, config.h_ahead, config.n_target)
 
-        self.train_index = self._get_valid_index(self.ir_train_data, self.mete_train_data, self.target_train_data)
-        self.validation_index = self._get_valid_index(self.ir_validation_data, self.mete_validation_data, self.target_validation_data)
-        self.test_index = self._get_valid_index(self.ir_test_data, self.mete_test_data, self.target_test_data)
+        #CAUTIOUS: the length of the ir_ttain_data and target_train_data may be differnet
+        #the length of mete_test_data may be more short
+        #and thus we must use the target data to compute the number
+
+        #assert the the data have been aggregate to the same length
+        #there is assertion to this property in this code
+        #so you need add some code if the assertion fails
+        assert len(ir_train_data) == len(target_train_data)
+        assert len(ir_validation_data) == len(target_validation_data)
+        assert len(ir_test_data) == len(target_test_data)
+
+        train_index = self._get_valid_index(ir_train_data, mete_train_data, target_train_data)
+        validation_index = self._get_valid_index(ir_validation_data, mete_validation_data, target_validation_data)
+        test_index = self._get_valid_index(ir_test_data, mete_test_data, target_test_data)
+
+        self.ir_train_data = ir_train_data[train_index]
+        self.ir_validation_data = ir_validation_data[validation_index]
+        self.ir_test_data = ir_test_data[test_index]
+
+        self.mete_train_data = mete_train_data[train_index]
+        self.mete_validation_data = mete_validation_data[validation_index]
+        self.mete_test_data = mete_test_data[test_index]
+
+        self.target_train_data = target_train_data[train_index]
+        self.target_validation_data = target_validation_data[validation_index]
+        self.target_test_data = target_test_data[test_index]
+
+        print len(self.ir_train_data)
+        print len(self.target_train_data)
+
+        #add some noise
+        #since some features are always a unchanged data which stddve is zero
+        #for there field, it do NOT need to add noise to them
+        expand_rate = 0
+        no_noise_index = (np.std(self.ir_train_data, axis=0) == 0)
+        self.ir_train_data = self._add_noise_to_data(self.ir_train_data, expand_rate, 30.0, no_noise_index)
+
+        no_noise_index = (np.std(self.mete_train_data, axis=0) == 0)
+        self.mete_train_data = self._add_noise_to_data(self.mete_train_data, expand_rate, 30.0, no_noise_index)
+
+        self.target_train_data = self._add_noise_to_data(self.target_train_data, expand_rate, 0.001)
 
         #concatenate all valid data
-        ir_raw_valid_data = np.concatenate((ir_train_raw_data[self.train_index], ir_validation_raw_data[self.validation_index], ir_test_raw_data[self.test_index]), axis=0)
-        mete_raw_valid_data = np.concatenate((mete_train_raw_data[self.train_index], mete_validation_raw_data[self.validation_index], mete_test_raw_data[self.test_index]), axis=0)
+        ir_valid_data = np.concatenate((self.ir_train_data, self.ir_validation_data, self.ir_test_data), axis=0)
+        mete_valid_data = np.concatenate((self.mete_train_data, self.mete_validation_data, self.mete_test_data), axis=0)
 
         #feature scale
-        ir_mean = np.mean(ir_raw_valid_data, axis=0)
-        ir_std = np.std(ir_raw_valid_data, axis=0)
+        ir_mean = np.mean(ir_valid_data, axis=0)
+        ir_std = np.std(ir_valid_data, axis=0)
         self.ir_train_data = (self.ir_train_data - ir_mean) / ir_std
         self.ir_validation_data = (self.ir_validation_data - ir_mean) / ir_std
         self.ir_test_data = (self.ir_test_data - ir_mean) / ir_std
 
-        mete_mean = np.std(mete_raw_valid_data, axis=0)
-        mete_std = np.std(mete_raw_valid_data, axis=0)
+        mete_mean = np.std(mete_valid_data, axis=0)
+        mete_std = np.std(mete_valid_data, axis=0)
+        mete_std[mete_std==0] = 1.0
         self.mete_train_data = (self.mete_train_data - mete_mean) / mete_std
         self.mete_validation_data = (self.mete_validation_data - mete_mean) / mete_std
         self.mete_test_data = (self.mete_test_data - mete_mean) / mete_std
 
-        #CAUTIOUS: the length of the ir_tarin_data and target_train_data may be differnet
+        #CAUTIOUS: the length of the ir_ttain_data and target_train_data may be differnet
         #the length of mete_test_data may be more short
         #and thus we must use the target data to compute the number
         self.train_num = len(self.target_train_data)
-        self.validataion_num = len(self.target_validation_data)
+        self.validation_num = len(self.target_validation_data)
         self.test_num = len(self.target_test_data)
 
         self.batch_size = config.batch_size
 
-        # self.index = np.random.random_integers(0, self.train_num-1, size=(self.batch_size))
         #print the dataset info
         print "Dataset info"
         print "="*80
-        print "valid train number:", len(self.train_index)
-        print "valid validation number:", len(self.validation_index)
-        print "valid test number", len(self.test_index)
+        print "train number:", self.train_num
+        print "validation number:", self.validation_num
+        print "test number", self.test_num
         print "batch size:", self.batch_size
         print "use", config.n_step, "hours to predict the next ", config.n_target, " consecutive hours"
         print "\n"
@@ -149,7 +195,7 @@ class Reader:
         @return mete_data_batch:  [batch_size, n_step, n_input]
         @return target_data_batch: [n_model, batch_size, n_target]
         """
-        index = np.random.choice(self.train_index, self.batch_size, replace=False)
+        index = np.random.choice(np.arange(self.train_num), self.batch_size, replace=False)
         ir_batch_data = self.ir_train_data[index]
         mete_batch_data = self.mete_train_data[index]
         target_batch_data = self.target_train_data[index]
@@ -162,9 +208,9 @@ class Reader:
         """
         @brief return the total dataset
         """
-        return self.ir_train_data[self.train_index], \
-                self.mete_train_data[self.train_index], \
-                self.target_train_data[self.train_index]
+        return self.ir_train_data, \
+                self.mete_train_data, \
+                self.target_train_data
 
     #The returned validataion and test set:
     #ir_data and mete_data: [batch_size, n_step, n_input], batch_size = validation_num/test_num
@@ -173,15 +219,15 @@ class Reader:
         """
         @brief return the total validation dataset
         """
-        return self.ir_validation_data[self.validation_index], \
-                self.mete_validation_data[self.validation_index], \
-                self.target_validation_data[self.validation_index]
+        return self.ir_validation_data, \
+                self.mete_validation_data, \
+                self.target_validation_data
 
     def get_test_set(self):
         """
         @brief return a test set in the specific test num
         @param test_num The number of test set to return
         """
-        return self.ir_test_data[self.test_index], \
-                self.mete_test_data[self.test_index], \
-                self.target_test_data[self.test_index]
+        return self.ir_test_data, \
+                self.mete_test_data, \
+                self.target_test_data
