089059dfbd074f0dcd00c3496306006c4c58994d
JackWang822 <wang_kejie@foxmail.com>
2017-01-17 11:56:45 +0800

    disable the model save and output save

--- src/solar_prediction.py
+++ src/solar_prediction.py
@@ -2,13 +2,14 @@
 __author__= 'WANG Kejie<wang_kejie@foxmail.com>'
 __date__ = '21/11/2016'
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
 import tensorflow as tf
 import numpy as np
 from config import Model_Config
 from reader import Reader
 from model import Model
 from util import MSE_And_MAE
-import os
 
 def fill_feed_dict(x_ir_placeholder, x_mete_placeholder, x_sky_cam_placeholder, y_, feed_data, modality):
     feed_dict = {}
@@ -65,19 +66,22 @@ def main(_):
     validation_last_loss = float('inf')
     best_test_result = None
 
-    save_folder_path = "./" + str(config.h_ahead) + "-" + str(config.h_ahead+config.n_target) + "/"
-    if not os.path.exists(save_folder_path):
-        os.mkdir(save_folder_path)
-    print save_folder_path
+    # save_folder_path = "./" + str(config.h_ahead) + "-" + str(config.h_ahead+config.n_target) + "/"
+    # if not os.path.exists(save_folder_path):
+    #     os.mkdir(save_folder_path)
+    # print save_folder_path
+    tf.logging.set_verbosity(tf.logging.FATAL)
 
-    with tf.Session() as sess:
+    tensor_config = tf.ConfigProto()
+    tensor_config.gpu_options.allow_growth = True
+    with tf.Session(config=tensor_config) as sess:
         # initialize all variables
         tf.global_variables_initializer().run()
 
-        path = tf.train.latest_checkpoint(save_folder_path)
-        if not (path is None):
-            save_path = saver.restore(sess, path)
-            print "restore model"
+        # path = tf.train.latest_checkpoint(save_folder_path)
+        # if not (path is None):
+        #     save_path = saver.restore(sess, path)
+        #     print "restore model"
 
         train_set = reader.get_train_set()
         validation_set = reader.get_validation_set()
@@ -87,22 +91,20 @@ def main(_):
         validation_feed = fill_feed_dict(x_ir, x_mete, x_sky_cam, y_, validation_set, modality)
         test_feed = fill_feed_dict(x_ir, x_mete, x_sky_cam, y_, test_set, modality)
 
+        np.set_printoptions(precision=4)
+        print sorted(np.array(test_set[-1]))
+
         for i in range(epoch_size):
             # test
             if i%config.test_step == 0:
                 #calculate the mse and mae
                 rmse, mae = do_eval(sess, [model.rmse, model.mae], test_feed)
-                print "Test RMSE: ", rmse, "Test MAE: ", mae
-
+                print "Test  RMSE: ", rmse, "Test  MAE: ", mae
                 rmse, mae = do_eval(sess, [model.rmse, model.mae], validation_feed)
-                print "Validation RMSE: ", rmse, "Validation MAE: ", mae
-
+                print "Valid RMSE: ", rmse, "Valid MAE: ", mae
                 rmse, mae = do_eval(sess, [model.rmse, model.mae], train_feed)
                 print "Train RMSE: ", rmse, "Train MAE: ", mae
-
                 print "sum of w: ", sess.run(model.w_sum)
-
-                print "\n"
                 print "bias of regression: ", sess.run(model.bias)
 
                 # test_result_path = "../output/" + str(config.regressor) + "/" + str(config.h_ahead) + "_" + str(config.h_ahead + config.n_target) + ".res"
@@ -121,9 +123,9 @@ def main(_):
             #validation
             validation_loss = do_eval(sess, model.loss, validation_feed)
 
-            if i%50 == 0 and i > 0 and validation_loss < validation_last_loss:
-                save_path = saver.save(sess, save_folder_path + "model.ckpt")
-                print "save the model to ", save_path
+            # if i%50 == 0 and i > 0 and validation_loss < validation_last_loss:
+            #     save_path = saver.save(sess, save_folder_path + "model.ckpt")
+            #     print "save the model to ", save_path
 
             #compare the validation with the last loss
             if validation_loss < validation_last_loss:
@@ -131,9 +133,9 @@ def main(_):
                 best_test_result = do_eval(sess, model.prediction, test_feed)
 
             # print "validation loss: ", validation_loss
-            if i%100 == 0 and i > 0:
-                save_path = saver.save(sess, "model.ckpt")
-                print "save the model"
+            # if i%100 == 0 and i > 0:
+            #     save_path = saver.save(sess, "model.ckpt")
+            #     print "save the model"
 
 
         # test_result_path = "../output/" + str(config.regressor) + "/" + str(config.h_ahead) + "_" + str(config.h_ahead + config.n_target) + ".res"
