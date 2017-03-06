a8d8a3d6e054e01eed8e6242213c2571700d6a53
JackWang822 <wang_kejie@foxmail.com>
2017-03-06 23:29:59 +0800

    add the pad data generate scipt

--- dataset/NREL_SSRL_BMS_SKY_CAM/input_data/pad_data_generate.py
+++ dataset/NREL_SSRL_BMS_SKY_CAM/input_data/pad_data_generate.py
@@ -1,11 +1,19 @@
 import datetime
 import numpy as np
 
+'''
+This file used to generate a file contains each hourly data to devote the sky image data
+The is three type of data:
+existed data: use its time to devote (integer) e.g. 2001010105 devote the image in 2000.01.01 05:00 exists
+missing data: the data between 05:00 and 19:00 which is not existed are devoted by MISSING_VALUE
+not captured data: since the sky image only captured between 05:00 and 19:00 and the other images are devoted by PAD_VALUE
+'''
+
 raw_data_path = '../raw_data/'
 exist_image_list_path = '../exist_image_list.csv'
 
 MISSING_VALUE = -99999
-
+PAD_VALUE = -11111
 
 year_start = 2000
 month_start = 11
@@ -25,7 +33,7 @@ while it < end_day:
     print it
     for hour in range(0, 24):
         if hour < 5 or hour > 19:
-            pad_data.append(-11111)
+            pad_data.append(PAD_VALUE)
         else:
             name = str(it.year).rjust(4, '0') + str(it.month).rjust(2, '0') + str(it.day).rjust(2, '0') + str(hour).rjust(2, '0')
             path = raw_data_path + str(it.year).rjust(4, '0') + '/' + str(it.month).rjust(2, '0') + '/' + str(it.day).rjust(2, '0') + '/' + name + '00.jpg'
