0f6281ea5efe01d54e87ad2ef92d68dfc3423e9a
JackWang822 <wang_kejie@foxmail.com>
2017-03-10 15:45:34 +0800

    fix the bugs in similar day reader

--- src/similar_day_fea_reader.py
+++ src/similar_day_fea_reader.py
@@ -5,8 +5,8 @@ HOUR_IN_A_DAY = 24
 class Similar_Day_Reader:
 
     def _get_day_tuple(self, time):
-        time = np.reshape(time, [-1, 24, time.shape[-1]])
-        time = time[:, 0, 0:3]
+        print time.shape
+        time = time[:, 0:3]
         return [tuple(x) for x in time]
 
     def __init__(self, similar_day_train_data_path, similar_day_validation_data_path, similar_day_test_data_path, time_train_data_path, time_validation_data_path, time_test_data_path):
@@ -23,16 +23,19 @@ class Similar_Day_Reader:
         data = np.reshape(data, [data.shape[0], data.shape[1], 1])
 
         time = np.concatenate((time_train_data, time_validation_data, time_test_data), axis=0)
+        time = np.reshape(time, [-1, HOUR_IN_A_DAY, time.shape[-1]])
+        time = time[:,0, :]
+
         time = self._get_day_tuple(time)
 
         self.dic = dict(zip(time, data))
 
-    def get_day_fea_map():
+    def get_day_fea_map(self):
         return self.dic
 
-    def get_data(train_time, validation_time, test_time):
+    def get_data(self, train_time, validation_time, test_time):
         train_time = self._get_day_tuple(train_time)
         validation_time = self._get_day_tuple(validation_time)
         test_time = self._get_day_tuple(test_time)
 
-        return np.array([dic[x] for x in train_time]), np.array([dic[x] for x in validation_time]), np.array([dic[x] for x in test_time])
+        return np.array([self.dic[x] for x in train_time]), np.array([self.dic[x] for x in validation_time]), np.array([self.dic[x] for x in test_time])
